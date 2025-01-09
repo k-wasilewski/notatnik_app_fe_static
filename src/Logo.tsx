abd4e937eb1f075be50571e3f9a4c793a1a8fde3
Wasilewski <e-jbww@gft.com>
2025-01-09 08:47:07 +0100

    added prettier

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => {
-  return <img src={require('../public/favicon.png')} width='100px' alt='logo'/>;
-}
+  return <img src={require('../public/favicon.png')} width="100px" alt="logo" />;
+};
 
 export default Logo;
